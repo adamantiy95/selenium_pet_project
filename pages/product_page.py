5f82cf5bc22a9f0a558d91ac5d089cb6c0aea213
adamakhm <akhadam00@gmail.com>
2025-05-12 16:15:20 +0300

    Сохранение перед pull

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,28 +1,54 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 class ProductPage(BasePage):
-    ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
     PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
     PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
-    SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success .alertinner strong")
-    BASKET_TOTAL = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages .alert-success .alertinner strong")
+    BASKET_TOTAL = (By.CSS_SELECTOR, ".alert-info .alertinner p strong")
 
-    def get_product_name(self):
-        return self.browser.find_element(*self.PRODUCT_NAME).text
+    def add_product_to_basket(self):
+        button = WebDriverWait(self.browser, 10).until(
+            EC.visibility_of_element_located(self.ADD_TO_BASKET_BUTTON)
+        )
+        button.click()
+
+    def get_product_name(self):  
+        element = WebDriverWait(self.browser, 10).until(
+            EC.visibility_of_element_located(self.PRODUCT_NAME)
+        )
+        return element.text
 
     def get_product_price(self):
-        return self.browser.find_element(*self.PRODUCT_PRICE).text
+        element = WebDriverWait(self.browser, 10).until(
+            EC.visibility_of_element_located(self.PRODUCT_PRICE)
+        )
+        return element.text
 
-    def add_to_basket(self):
-        self.browser.find_element(*self.ADD_TO_BASKET).click()
+    def get_success_message(self):
+        element = WebDriverWait(self.browser, 10).until(
+            EC.visibility_of_element_located(self.SUCCESS_MESSAGE)
+        )
+        return element.text
 
-    def should_be_success_message(self, expected_name):
-        success_message = self.browser.find_element(*self.SUCCESS_MESSAGE).text
-        assert expected_name == success_message, \
-        f"Ожидаемое название товара '{expected_name}' в успешном сообщении, но получено '{success_message}'"
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*self.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
 
-    def should_be_correct_basket_total(self, expected_price):
-        basket_total = self.browser.find_element(*self.BASKET_TOTAL).text
-        assert expected_price == basket_total, \
-        f"Ожидаемая итоговая сумма '{expected_price}', но получено '{basket_total}'"
+    def success_message_should_disappear(self):
+        assert self.is_disappeared(*self.SUCCESS_MESSAGE), \
+            "Success message is not disappeared, but should be"
+    
+    def get_basket_total(self):
+        return self.browser.find_element(*self.BASKET_TOTAL).text
+
+    def should_be_success_message_with_product_name(self):
+        assert self.get_product_name() == self.get_success_message(), \
+            "Product name in success message is incorrect"
+
+    def should_be_correct_basket_total(self):
+        assert self.get_product_price() == self.get_basket_total(), \
+            "Basket total is incorrect"
--- test_product_page.py
+++ test_product_page.py
@@ -1,23 +1,39 @@
 from pages.product_page import ProductPage
 import math
 import pytest
+from selenium import webdriver
 
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def test_guest_can_add_product_to_basket(browser, link):
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+@pytest.fixture
+def browser():
+    browser = webdriver.Chrome()
+    yield browser
+    browser.quit()
+
+
+# def test_guest_can_add_product_to_basket(browser):
+#    page = ProductPage(browser, link)
+#    page.open()
+#    page.add_product_to_basket()
+#    page.should_be_success_message_with_product_name()
+#    page.should_be_correct_basket_total()
+
+@pytest.mark.xfail(reason="fixing this bug right now")
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
-    product_name = page.get_product_name()
-    product_price = page.get_product_price()
-    page.add_to_basket()
-    page.solve_quiz_and_get_code()
-    page.should_be_success_message(product_name)
-    page.should_be_correct_basket_total(product_price)
+    page.add_product_to_basket()
+    page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+@pytest.mark.xfail(reason="fixing this bug right now")
+def test_message_disappeared_after_adding_product_to_basket(browser): 
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.success_message_should_disappear()
